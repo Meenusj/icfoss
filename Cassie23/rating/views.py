c015101771aa0fd181a858028c78fe0a07e5165d
Akshara-P-Vijayan <aksharapvijayan@gmail.com>
2023-03-18 19:48:02 +0530

    19.47

---
+++ Cassie23/rating/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    a=["1.how likely was the staffs behaviour",
+    "2.how was the toilet facility and cleanliness",
+    "3.Did accomadation facility is easily available in there",
+    "4.did the information we given was usefull"]
+    return render(request, 'rating.html', {'a':a})
