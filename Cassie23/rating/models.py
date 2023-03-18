a979f8d6608618b4b862609ade2acbc5759dcb7d
aiswaryabiju <aiswaryabiju234@gmail.com>
2023-03-19 04:04:13 +0530

    04:04

--- Cassie23/rating/models.py
+++ Cassie23/rating/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class RatingModel(models.Model):
+    avgrating:str
+    behaviour:int
+    toiletcleanliness:int
+    inhousestay:bool
+    disableperson_friendly:bool
+    medical_emergency:bool
+    food_facility:int
+    about:str
+    
