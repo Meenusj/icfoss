b9aedf3484c8fcf312ce0c0cf4299c7e39eb9f8d
aiswaryabiju <aiswaryabiju234@gmail.com>
2023-03-18 09:14:08 +0530

    view1

--- Cassie23/user/views.py
+++ Cassie23/user/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,3 +10,23 @@ def home(request):
 
 def authenticate(request):
     return render(request, 'authenticate.html')
+
+def login(request):
+    if request.method=="POST":
+        username=request.POST['email_address']
+        password=request.POST['password']
+        user=auth.authenticate(username=username,password=password)
+        
+        if user is not None:
+            auth.login(request,user)
+            return render(request, 'dashboard.html')
+        else:
+            messages.info(request,"Invalid credentials")
+            return redirect('authenticate')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+
+
+
