2436f3c3e2240764eccc7b289c31cef388b2226d
aiswaryabiju <aiswaryabiju234@gmail.com>
2023-03-19 07:46:07 +0530

    2.7

--- Cassie23/rating/views.py
+++ Cassie23/rating/views.py
@@ -5,7 +5,7 @@ def home(request):
     a=["1.how likely was the staffs behaviour",
     "2.how was the toilet facility and cleanliness",
     "3.Did accomadation facility is easily available in there",
-    "4.did the information we given was usefull"]
+    "4.did tthere is any hospital facility"]
     return render(request, 'rating.html', {'a':a})
 
 
